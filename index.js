7d0b2cb714875aadf0ed467d358701f3c799fa16
Alex <119153127+Amack2@users.noreply.github.com>
2023-02-01 23:50:36 -0800

    etch a sketch drawing function working

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const mainContainer = document.querySelector('#etchASketchMainContainer');
-/*
-if (resp)
-*/
 
 
-y= prompt('what is y');
+
+y = prompt('what is y');
 x = prompt('what is x');
-let a = x*y;
+let a = x * y;
 count = 1;
 
 
 
-function timesToLoop() {
-   for ( let i = 0; i < a; i++){
-   console.log(count);
-    count = count +1; 
-    const square = document.createElement('div');
-    square.classList.add('square');
-    mainContainer.appendChild(square); 
-  };
-     
-};
+function createSquares() {
+    for (let i = 0; i < a; i++) {
+        console.log(count);
+        count = count + 1;
+        const square = document.createElement('div');
+        square.classList.add('square');
+        mainContainer.appendChild(square);
+        square.addEventListener('mouseover', function (e) {
+            square.classList.add('hovering');
+        });
+    };
 
-timesToLoop();
+};
+createSquares();
+
+
+
+
+//const squares = document.querySelectorAll('.square');
+//squares.forEach(square)
+
+//square.classList.add('hovering');
+//setTimeout(function () {
+
+
+
+//})
+
+//}
+
+
+//)
+
+
+
+
+
 
 
 
